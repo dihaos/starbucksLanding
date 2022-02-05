b941325088d8babdd2e2ab3e4ea07ca6c7510032
dihaos <you@example.com7390540@mail.ru>
2022-02-06 02:56:35 +0300

    додавил адаптивность под смартфоны

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ coffee.onmouseover = (e) => {
         document.getElementById('circle').style.background = e.target.id
     }
 }
+
+toggleMenu = () => {
+    const menuToggle = document.querySelector('.toggle');
+    const navigation = document.querySelector('.navigation')
+    
+    menuToggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
